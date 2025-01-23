89a6be44b8ed67cb785accc184e1cfec57f8e3c8
mkhi238 <100494918+mkhi238@users.noreply.github.com>
2025-01-23 14:18:26 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -47,6 +47,7 @@ def heur_manhattan_distance(state):
     # Your function should return a numeric value; this is the estimate of the distance to the goal.
     return 0  # CHANGE THIS
 
+
 def fval_function(sN, weight):
     # IMPLEMENT
     """
@@ -57,7 +58,8 @@ def fval_function(sN, weight):
     @param float weight: Weight given by Anytime Weighted A star
     @rtype: float
     """
-    return 0 #CHANGE THIS
+    fval = sN.gval + weight * sN.hval
+    return fval
 
 # SEARCH ALGORITHMS
 def weighted_astar(initial_state, heur_fn, weight, timebound):
@@ -66,6 +68,9 @@ def weighted_astar(initial_state, heur_fn, weight, timebound):
     '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
     '''OUTPUT: A goal state (if a goal is found), else False as well as a SearchStats object'''
     '''implementation of weighted astar algorithm'''
+    search_engine = 
+
+
     return None, None  # CHANGE THIS
 
 def iterative_astar(initial_state, heur_fn, weight=1, timebound=5):  # uses f(n), see how autograder initializes a search line 88
