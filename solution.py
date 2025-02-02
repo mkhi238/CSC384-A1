059ffe7c498019646a405bbc19f839d556e1ccf7
mkhi238 <100494918+mkhi238@users.noreply.github.com>
2025-02-01 23:59:38 -0500

    Final Changes

--- solution.py
+++ solution.py
@@ -33,7 +33,9 @@ def heur_alternate(state):
     make said move. 
 
     Following this, my function will find the Manhattan distance from all remaining boxes, and select the box to move with the closest space (similar to the regular Manhattan distance
-    we made before)
+    we made before). My function also check to see, for goals and boxes in the same parallel (meaning that they share a same x or y coordinate) and for which the goal is the closest to the box
+    I make sure to check if there is an obstacle or box in the way. If so, I add 1 to the score (I believe this maintains admissability because regardless we would have needed to have made another 
+    move to bypass the obstacle or box). 
 
     Finally, my function adds in the distance to move the robot to the box, such that although the idea is to greedily select the closest box, boxes too far away will be discouraged. 
 
@@ -70,15 +72,16 @@ def heur_alternate(state):
         
 
     #3. CALCULATING MANHATTAN DISTANCE
-    for box in state.boxes:
+    for b in boxes:
         min_distance = 1000000000    #Set the minimum distance to large number
         closest_goal = 0     
 
         for goal in unassigned_goals:
-            dist = abs(box[0] - goal[0]) + abs(box[1] - goal[1])    #Calculate Manhattan distance
+            dist = abs(b[0] - goal[0]) + abs(b[1] - goal[1])    #Calculate Manhattan distance
 
-            if box_blocking_goal(box, goal, boxes, obstacles):
-                dist += 1
+            #Check if, for a given goal/box combination where the box and goal are in a straight line horizontally or vertically, check if there is a blocker in the way
+            if object_or_box_in_the_way(b, goal, boxes, obstacles) == True:
+                dist += 1 #if there is a blocker, add 1 (I experimented with different values to add [0.5,1,2] and found 1 to be the best, along with it being a value that maintains admissability (we will be guarneteed to make at least 1 more move))
 
             #Greedily assign boxes to the nearest goal state
             if dist < min_distance:                                 
@@ -95,22 +98,6 @@ def heur_alternate(state):
     total_distance += distance_from_robot_to_box(state) #Find distance from the robot to the box and add to total distance (just updating the heuristic value, still greedily only accounting for box to goal distance in calculation)
     return total_distance 
 
-def box_blocking_goal(box, goal, boxes, obstacles):
-    x1, y1 = box
-    x2, y2 = goal
-
-    if y1 == y2:
-        for x in range(min(x1, x2) + 1, max(x1, x2)):  
-            if (x, y1) in boxes:
-                return True
-
-    elif x1 == x2:
-        for y in range(min(y1, y2) + 1, max(y1, y2)):  
-            if (x1, y) in boxes:
-                return True
-            
-
-    return False  
 
 #FUNCTION TO ENUMERATE THE LOCATION OF ALL WALLS AND OBSTACLES
 def wall_set(width, height, obstacles):
@@ -230,20 +217,6 @@ def obs_stuck(box, wall_set, obstacles):
             LO_walls_above2 or LO_walls_below2 or RO_walls_above2 or RO_walls_below2 or TO_walls_left2 or TO_walls_right2 or BO_walls_left2 or BO_walls_right2,
             LO_walls_above3 or LO_walls_below3 or RO_walls_above3 or RO_walls_below3 or TO_walls_left3 or TO_walls_right3 or BO_walls_left3 or BO_walls_right3)
 
-def get_minimal_detour(box, goal, obstacles, boxes):
-
-    detour_cost = 0
-    x1, y1 = box
-    x2, y2 = goal
-    
-    # Check if any obstacle is directly in the path
-    for x in range(min(x1, x2), max(x1, x2) + 1):
-        for y in range(min(y1, y2), max(y1, y2) + 1):
-            if (x, y) in obstacles or ((x, y) in boxes and (x, y) != box):
-                # If obstacle is in direct path, must take at least 1 step detour
-                detour_cost = 1
-                break
-    return detour_cost
 
 #FUNCTION TO DETECT IF A BOX IS NEAR A CORNER (CREATED BY WALLS)
 def corner_detection(box, wall_set, width, height):
@@ -278,10 +251,6 @@ def distance_from_robot_to_box(state):
     for box in state.boxes:
         if box not in state.storage:
             unplaced_boxes.append(box)
-
-    #If there is no unplaced boxes for whatever reason
-    if len(unplaced_boxes) == 0:
-        return 0
     
     #Iterate through robots, find the robot with the minimum distance and add it to the calculation
     min_dist = 1000000000
@@ -293,6 +262,39 @@ def distance_from_robot_to_box(state):
             if dist < min_dist:
                 min_dist = dist
     return min_dist
+
+#FUNCTION TO DETERMINE IF SOMETHING (OBSTACLE/BOX) IS IN THE WAY IN A STRAIGHT LINE FROM THE BOX TO THE GOAL 
+# Im using this function to see, if the goal is a straight line from us, whether or not something is in the way. 
+# If so, we know that we need to adds at least plus one. Becaue of this fact, adding +1 to our total distance still maintains admissablility
+# This is because any which way, if there is something that is in the way, then we are going to have to make at least one extra move to make a detour
+# As you can see in the function itself, this only works if the goal and the box are on the same parallel (x or y) - otherwise, we cannot determine anything (admissably)
+def object_or_box_in_the_way(box, goal, boxes, obstacles):
+    #Get the location of the box and the goal
+    xbox = box[0]
+    ybox = box[1]
+    xgoal = goal[0]
+    ygoal = goal[1]
+    in_the_way = False
+
+    #if the box and goal is on the same y parallel
+    if ybox == ygoal:
+        #look forward from the goal or box (whichever is smaller) to the space before the goal (i did this to make it more robust, and found through experimentation
+        #of different ranges that this worked the best)
+        for i in range(min(xbox, xgoal) + 1, max(xbox, xgoal)-1):  
+            if (i, ybox) in boxes or (i,ybox) in obstacles: #if there are obstacles or boxes in the way, return True (we will need to make an extra step)
+                return in_the_way == True
+            
+    #if the box and goal is on the same x parallel
+    elif xbox == xgoal:
+        #look forward from the goal or box (whichever is smaller) to the space before the goal (i did this to make it more robust, and found through experimentation
+        #of different ranges that this worked the best)
+        for j in range(min(ybox, ygoal) + 1, max(ybox, ygoal)-1):  
+            if (xbox, j) in boxes or (xbox,j) in obstacles: #if there are obstacles or boxes in the way, return True (we will need to make an extra step)
+                return in_the_way == True
+    
+    #if not, return False (will not need to add +1)
+    return in_the_way 
+
 
 def heur_zero(state):
     # We want an admissible heuristic, which is an optimistic heuristic.
@@ -357,7 +359,7 @@ def iterative_astar(initial_state, heur_fn, weight=1, timebound=5):  # uses f(n)
     search_engine = SearchEngine(strategy = 'custom', cc_level = 'full')
     #Inialize costbound (g(n), h(n), f(n)) with some arbitariliy high numbers, as defined in section 3.0
     costbound = (100000000,100000000,100000000) 
-    weight = 15
+    weight = 15 #initalize weight to something high
     start_time = os.times()[0]  #intialize start time
     best = None #initalize best solution (none so far)
     best_stats = None #initalize best stats (none so far)
@@ -373,10 +375,10 @@ def iterative_astar(initial_state, heur_fn, weight=1, timebound=5):  # uses f(n)
             best = goal_found #Update best solution
             best_stats = stats #Update best solution stats
             remaining_time = timebound - (os.times()[0] - start_time)
-            weight *= 0.7  # Reduce weight slower
+            weight *= 0.7  # Reduce weight slower if we find a solution
         
         else:
-            weight *= 0.5  # Reduce weight aggressively
+            weight *= 0.5  # Reduce weight aggressively if we dont find a solution
     
     #If we done find a soluiton in time
     if best == None:
